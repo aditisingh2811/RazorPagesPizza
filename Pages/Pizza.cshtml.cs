d9e59a84715a01db45a3d19c031b2ec563572b3f
aditisingh2811 <aditi28singh01@gmail.com>
2022-07-16 01:08:46 +0530

    first commit

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesPizza.Pages
+{
+    using RazorPagesPizza.Models;
+    using RazorPagesPizza.Services;
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+        public List<Pizza> pizzas = new();
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if(pizza.IsGlutenFree)
+            return "Gluten Free";
+            return "Not Gluten Free";
+        }
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+        [BindProperty]
+        public Pizza NewPizza { get; set;} = new();
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+    }
+}
